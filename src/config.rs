817222f73d5b3f14e6d32d1a3ddb6d319e402140
Patryk Ozga <patryk.ozga@outlook.com>
2016-11-21 19:37:32 -0800

    add config settings

--- src/config.rs
+++ src/config.rs
@@ -6,4 +6,5 @@ pub struct MinerConfiguration {
     pub plot_folders: Option<Vec<String>>,
     pub threads_per_folder: Option<u32>,
     pub max_deadline: Option<u32>,
+    pub plot_buffer_size: Option<u32>
 }
