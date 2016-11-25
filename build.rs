6048d3603fcaa473c934a29935889ca85ca5cb9d
Patryk Ozga <patryk.ozga@outlook.com>
2016-11-25 01:34:45 -0600

    add default config to build drop

--- build.rs
+++ build.rs
@@ -1,5 +1,14 @@
 extern crate gcc;
 
+use std::{env, fs, path};
+
 fn main() {
+    // compile libsph
     gcc::compile_library("libsph_shabal.a", &["lib/shabal.c"]);
+
+    // copy default config to target
+    let resource_file = "config.json";
+    let source_path = env::current_dir().unwrap().join(resource_file);
+    let output_path = path::Path::new(env!("OUT_DIR")).join(resource_file);
+    fs::copy(&source_path, &output_path).unwrap();
 }
